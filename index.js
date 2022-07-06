609a10d36ed9cc8ae504c20da408892085560a53
Vasanth1650 <vasanth16756@gmail.com>
2022-07-06 18:43:58 +0530

    wda

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://nikeworld.herokuapp.com/",
     methods: ["GET", "POST"],
   },
 });
@@ -31,6 +31,3 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
-  console.log("SERVER RUNNING");
-});
